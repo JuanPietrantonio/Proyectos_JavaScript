6a7d3aab8054117b4c70de23e141002df3db5cc7
JuanPietrantonio <juampipietra@gmail.com>
2022-09-07 23:30:55 -0300

    proyecto final 4to push

--- Proyecto Final/javascript.js
+++ Proyecto Final/javascript.js
@@ -79,6 +79,7 @@ function imprimirInfo () {
     const impresionInfo = document.createElement('ul');
     divContainterInfo.innerHTML = "";
     impresionInfo.innerHTML = `
+    <h3>Infomacion general</h3>
     <li>Promedio de ventas: ${promedio}</li>
     <li>Distribucion con Promo / sin Promo: ${sinPromo}%/${conPromo}%</li>
     `
@@ -119,8 +120,14 @@ function imprimirObjetivo1 () {
     const impresionObjetivo1 = document.createElement('ul');
     divContainterObjetivo1.innerHTML = "";
     impresionObjetivo1.innerHTML = `
-    <li>Ventas para proxima escala: ${ventasFaltantesProxEscala}</li>
+    <h3>Siguiente escala: ${proxEscala}</h3>
+    <li>Ventas para alcanzar proxima escala: ${ventasFaltantesProxEscala}</li>
     <li>Ventas diarias para proxima escala: ${ventasPorDiaProxEscala}</li>
+    <li>Comision: ${proxComision}</li>
+    <br>
+    <h3>20% extra para asegurar escala: ${(proxEscala*1.2)}</h3>
+    <li>Ventas para asegurar proxima escala: ${((proxEscala*1.2)-ventasTotal)}</li>
+    <li>Ventas diarias para asegurar escala: ${((proxEscala*1.2)-ventasTotal)/datosStorage[3]}</li>
     <li>Comision: ${proxComision}</li>
     `
     divContainterObjetivo1.append(impresionObjetivo1);
@@ -130,17 +137,17 @@ function imprimirObjetivo2 () {
     const datosStorage = JSON.parse(localStorage.getItem('datos'));
     const objetivoStorage = JSON.parse(localStorage.getItem('objetivo'))
     let ventasFaltantesProxEscala = parseInt(datos[4]) - (parseInt(datosStorage[1]) +  parseInt(datosStorage[2]));
-    let ventasPorDiaProxEscala = parseInt(ventasFaltantesProxEscala / datosStorage[3])
-    //aca la logica de la comision segun obj
-    let posibleComision = parseInt(objetivoStorage[0].comision)
+    let ventasPorDiaProxEscala = parseFloat(ventasFaltantesProxEscala / datosStorage[3])
+    let proxComision = (ventasTotal < objetivoStorage[0].escala) ? parseInt(objetivoStorage[0].comision) : (ventasTotal >= objetivoStorage[0].escala && ventasTotal < objetivoStorage[1].escala ) ? parseInt(objetivoStorage[1].comision) : (ventasTotal >= objetivoStorage[1].escala && ventasTotal < objetivoStorage[2].escala) ? parseInt(objetivoStorage[2].comision) : (ventasTotal >= objetivoStorage[2].escala && ventasTotal < objetivoStorage[3].escala) ? parseInt(objetivoStorage[3].comision) : parseInt(datos[4]);
 
     const divContainterObjetivo2 = document.getElementById('container-2-objetivo-detalle2');
     const impresionObjetivo2 = document.createElement('ul');
     divContainterObjetivo2.innerHTML = "";
     impresionObjetivo2.innerHTML = `
+    <h3>Objetivo personal</h3>
     <li>Ventas para proxima escala: ${ventasFaltantesProxEscala}</li>
     <li>Ventas diarias para proxima escala: ${ventasPorDiaProxEscala}</li>
-    <li>Comision: ${posibleComision}</li>
+    <li>Comision: ${proxComision}</li>
     `
     divContainterObjetivo2.append(impresionObjetivo2);
 }
@@ -149,3 +156,5 @@ function imprimirObjetivo2 () {
 //HACER PRUEBAS DE LOGICA CUANDO VENDO MAS DE 34 VENTAS Y LO MISMO CON OBJETIVO PERSONAL, FUNCION FLECHA PARA CALCULAR LA COMISION 
 
 //VER SI SE PUEDE AGREGAR UN "VALOR VENTA" IGUAL QUE LOS IF ELSE DE LAS ESCALAS, AGREGAR AL DATA.JSON EL VALOR POR VENTA SEGUN CADA ESCALA Y LISTO YA TENEMOS LO NECESARIO PARA CALCULAR LA PLATA EXACTA QUE SE PUEDEN LLEVAR CON SU ESCALA MAS LAS VENTAS INDIVIDUALES
+
+//terminar objetivo personal con 20% extra
