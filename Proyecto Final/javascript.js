eebab2c8625a89a9f50ce304df3670a1ee4d97b7
JuanPietrantonio <juampipietra@gmail.com>
2022-09-11 20:29:19 -0300

    proyecto final

--- Proyecto Final/javascript.js
+++ Proyecto Final/javascript.js
@@ -19,6 +19,12 @@ function activarFetch () {
         diasVigenciaFetch(data);
         objetivoFetch(data);
     })
+    fetch ('./vendedor.json')
+        .then (res => res.json())
+        .then (vend => {
+        listaVendedores(vend);
+    })
+
 }
 
 function diasVigenciaFetch (data) {
@@ -31,14 +37,21 @@ function objetivoFetch (data) {
     }
 }
 
+function listaVendedores(vend) {
+    const listaId = document.getElementById('vendedor');
+    for (vend of vend ) {
+        const listaOpciones = document.createElement('option');
+        listaOpciones.value = vend.ID;
+        listaOpciones.text = vend.vendedor;
+        listaId.append(listaOpciones);
+    }
+    
+}
+
 function pantallaInicial () {
     datosStorage = localStorage.getItem('datos') || null;
     datosStorage == null ? borrarDatos() : cargarDatosIniciales();
     function cargarDatosIniciales () {
-        /*const datosStorage = JSON.parse(localStorage.getItem('datos'));
-        const diasVigencia = localStorage.getItem('diasVigencia');
-        const objetivoStorage = JSON.parse(localStorage.getItem('objetivo'));*/
-        imprimirNombre();
         imprimirInfo ();
         imprimirObjetivo1();
         imprimirObjetivo2();
@@ -49,65 +62,81 @@ pantallaInicial();
 
 function borrarDatos () {
     const divContainterInfo = document.getElementById('container-2-info');
-    const impresionInfo = document.createElement('ul');
+    const impresionInfo = document.createElement('li');
     divContainterInfo.innerHTML = "";
     impresionInfo.innerHTML = `
     <h3>Infomacion general</h3>
-    <li>Total de ventas: -</li>
-    <li>Promedio de ventas: -</li>
-    <li>Distribucion con Promo / sin Promo: -%/-%</li>
-    <li>Escala actual o la mas cercana: -</li>
-    <li>Comision actual o la mas cercana: -</li>
+    <ul>Total de ventas: -</ul>
+    <ul>Promedio de ventas: -</ul>
+    <ul>Distribucion con Promo / sin Promo: -%/-%</ul>
+    <ul>Escala actual o la mas cercana: -</ul>
+    <ul>Comision actual o la mas cercana: -</ul>
     `
     divContainterInfo.append(impresionInfo);
 
     const divContainterObjetivo1 = document.getElementById('container-2-objetivo-detalle1');
-    const impresionObjetivo1 = document.createElement('ul');
+    const impresionObjetivo1 = document.createElement('li');
     divContainterObjetivo1.innerHTML = "";
     impresionObjetivo1.innerHTML = `
     <h3>Siguiente escala: -</h3>
-    <li>Ventas para alcanzar proxima escala: -</li>
-    <li>Ventas diarias para proxima escala: -</li>
-    <li>Comision: -</li>
+    <ul>Ventas para alcanzar proxima escala: -</ul>
+    <ul>Ventas diarias para proxima escala: -</ul>
+    <ul>Comision: -</ul>
     <br>
     <h3>20% extra para asegurar escala: -</h3>
-    <li>Ventas para asegurar proxima escala: -</li>
-    <li>Ventas diarias para asegurar escala: -</li>
-    <li>Comision: -</li>
+    <ul>Ventas para asegurar proxima escala: -</ul>
+    <ul>Ventas diarias para asegurar escala: -</ul>
+    <ul>Comision: -</ul>
     `
     divContainterObjetivo1.append(impresionObjetivo1);
 
     const divContainterObjetivo2 = document.getElementById('container-2-objetivo-detalle2');
-    const impresionObjetivo2 = document.createElement('ul');
+    const impresionObjetivo2 = document.createElement('li');
     divContainterObjetivo2.innerHTML = "";
     impresionObjetivo2.innerHTML = `
     <h3>Objetivo personal: -</h3>
-    <li>Ventas para objetivo: -</li>
-    <li>Ventas diarias para objetivo: -</li>
-    <li>Comision: -</li>
+    <ul>Ventas para objetivo: -</ul>
+    <ul>Ventas diarias para objetivo: -</ul>
+    <ul>Comision: -</ul>
     <br>
     <h3>20% extra para asegurar objetivo: -</h3>
-    <li>Ventas para asegurar objetivo: -</li>
-    <li>Ventas diarias para asegurar objetivo: -</li>
-    <li>Comision: -</li>
+    <ul>Ventas para asegurar objetivo: -</ul>
+    <ul>Ventas diarias para asegurar objetivo: -</ul>
+    <ul>Comision: -</ul>
     `
     divContainterObjetivo2.append(impresionObjetivo2);
+    localStorage.clear();
 }
 
 
 function cargarDatos () {
-    const nombre = document.getElementById("nombre").value;
+    const nombre = document.getElementById("vendedor");
     const sinPromo = document.getElementById("sinPromo").value;
     const conPromo = document.getElementById("conPromo").value;
     const diasRestantes = parseInt(document.getElementById("diasRestantes").value);
     const objetivoPersonal = document.getElementById("objetivoPersonal").value;
+    let ventasTotal = parseInt(sinPromo) + parseInt(conPromo);
+    if (isNaN(sinPromo) ||  sinPromo <0 ||  isNaN(conPromo) || conPromo <0 || isNaN(diasRestantes) || diasRestantes <0 || isNaN(objetivoPersonal) || objetivoPersonal<ventasTotal) {
+        swal({
+            title: "Revisa los datos cargados",
+            text: "",
+            icon: "error",
+          });
+          pantallaInicial();
+          return;
+    }
+    
     while(datos.length > 0){datos.pop()}
-    datos.push(nombre, sinPromo, conPromo, diasRestantes, objetivoPersonal);  
+    datos.push(nombre.value, sinPromo, conPromo, diasRestantes, objetivoPersonal);  
     sincronizarStorage ();  
     imprimirNombre();
     imprimirInfo ();
     imprimirObjetivo1();
     imprimirObjetivo2();
+    Toastify({
+        text: "Datos actualizados",
+        duration: 2000
+    }).showToast();
 }
 
 
@@ -121,8 +150,7 @@ function imprimirNombre () {
     const divContainerNombre = document.getElementById('container-2-nombre');
     const impresionNombre = document.createElement('h2');
     divContainerNombre.innerHTML = "";
-    const datosStorage = JSON.parse(localStorage.getItem('datos'));
-    impresionNombre.innerHTML = `${datosStorage[0]}`
+    impresionNombre.innerHTML = `${datos[0]}`
     divContainerNombre.append(impresionNombre);
 }
 
@@ -137,15 +165,15 @@ function imprimirInfo () {
     let escalaActual = (ventasTotal < objetivoStorage[0].escala) ? parseInt(objetivoStorage[0].escala) : (ventasTotal >= objetivoStorage[0].escala && ventasTotal < objetivoStorage[1].escala ) ? parseInt(objetivoStorage[0].escala) : (ventasTotal >= objetivoStorage[1].escala && ventasTotal < objetivoStorage[2].escala) ? parseInt(objetivoStorage[1].escala) : (ventasTotal >= objetivoStorage[2].escala && ventasTotal < objetivoStorage[3].escala) ? parseInt(objetivoStorage[2].escala) : ventasTotal;
     let proxComision = (ventasTotal < objetivoStorage[0].escala) ? parseInt(objetivoStorage[0].comision) : (ventasTotal >= objetivoStorage[0].escala && ventasTotal < objetivoStorage[1].escala ) ? parseInt(objetivoStorage[0].comision) : (ventasTotal >= objetivoStorage[1].escala && ventasTotal < objetivoStorage[2].escala) ? parseInt(objetivoStorage[1].comision) : (ventasTotal >= objetivoStorage[2].escala && ventasTotal < objetivoStorage[3].escala) ? parseInt(objetivoStorage[2].comision) :  parseInt(objetivoStorage[3].comision);
     const divContainterInfo = document.getElementById('container-2-info');
-    const impresionInfo = document.createElement('ul');
+    const impresionInfo = document.createElement('li');
     divContainterInfo.innerHTML = "";
     impresionInfo.innerHTML = `
-    <h3>Infomacion general</h3>
-    <li>Total de ventas: ${ventasTotal}</li>
-    <li>Promedio de ventas: ${promedio}</li>
-    <li>Distribucion con Promo / sin Promo: ${sinPromo}%/${conPromo}%</li>
-    <li>Escala actual o la mas cercana: ${escalaActual}</li>
-    <li>Comision actual o la mas cercana: ${proxComision}</li>
+    <h3> Infomacion general</h3>
+    <ul>Total de ventas: ${ventasTotal}</ul>
+    <ul>Promedio de ventas: ${promedio.toFixed(2)}</ul>
+    <ul>Distribucion con Promo / sin Promo: ${sinPromo.toFixed()}%/${conPromo.toFixed()}%</ul>
+    <ul>Escala actual o la mas cercana: ${escalaActual}</ul>
+    <ul>Comision actual o la mas cercana: ${proxComision}</ul>
     `
     divContainterInfo.append(impresionInfo);
 }
@@ -164,18 +192,18 @@ function imprimirObjetivo1 () {
     let ventasPorDiaProxEscala = parseFloat(ventasFaltantesProxEscala / parseInt(datosStorage[3]))
     
     const divContainterObjetivo1 = document.getElementById('container-2-objetivo-detalle1');
-    const impresionObjetivo1 = document.createElement('ul');
+    const impresionObjetivo1 = document.createElement('li');
     divContainterObjetivo1.innerHTML = "";
     impresionObjetivo1.innerHTML = `
     <h3>Siguiente escala: ${proxEscala}</h3>
-    <li>Ventas para alcanzar proxima escala: ${ventasFaltantesProxEscala}</li>
-    <li>Ventas diarias para proxima escala: ${ventasPorDiaProxEscala}</li>
-    <li>Comision: ${proxComision}</li>
+    <ul>Ventas para alcanzar proxima escala: ${ventasFaltantesProxEscala}</ul>
+    <ul>Ventas diarias para proxima escala: ${ventasPorDiaProxEscala.toFixed(2)}</ul>
+    <ul>Comision: ${proxComision}</ul>
     <br>
     <h3>20% extra para asegurar escala: ${parseInt((proxEscala*1.2))}</h3>
-    <li>Ventas para asegurar proxima escala: ${parseInt(((proxEscala*1.2)-ventasTotal))}</li>
-    <li>Ventas diarias para asegurar escala: ${((proxEscala*1.2)-ventasTotal)/datosStorage[3]}</li>
-    <li>Comision: ${proxComision}</li>
+    <ul>Ventas para asegurar proxima escala: ${parseInt(((proxEscala*1.2)-ventasTotal))}</ul>
+    <ul>Ventas diarias para asegurar escala: ${(((proxEscala*1.2)-ventasTotal)/datosStorage[3]).toFixed(2)}</ul>
+    <ul>Comision: ${proxComision}</ul>
     `
     divContainterObjetivo1.append(impresionObjetivo1);
 }
@@ -190,18 +218,18 @@ function imprimirObjetivo2 () {
     let objComision = (datosStorage[4] < objetivoStorage[0].escala) ? parseInt(objetivoStorage[0].comision) : (datosStorage[4] >= objetivoStorage[0].escala && datosStorage[4] < objetivoStorage[1].escala ) ? parseInt(objetivoStorage[0].comision) : (datosStorage[4] >= objetivoStorage[1].escala && datosStorage[4] < objetivoStorage[2].escala) ? parseInt(objetivoStorage[1].comision) : (datosStorage[4] >= objetivoStorage[2].escala && datosStorage[4] < objetivoStorage[3].escala) ? parseInt(objetivoStorage[2].comision) : parseInt(objetivoStorage[3].comision + (datosStorage[4] - objetivoStorage[3].escala)*400)
 
     const divContainterObjetivo2 = document.getElementById('container-2-objetivo-detalle2');
-    const impresionObjetivo2 = document.createElement('ul');
+    const impresionObjetivo2 = document.createElement('li');
     divContainterObjetivo2.innerHTML = "";
     impresionObjetivo2.innerHTML = `
     <h3>Objetivo personal: ${datosStorage[4]}</h3>
-    <li>Ventas para objetivo: ${ventasFaltantesProxEscala}</li>
-    <li>Ventas diarias para objetivo: ${ventasPorDiaProxEscala}</li>
-    <li>Comision: ${objComision}</li>
+    <ul>Ventas para objetivo: ${ventasFaltantesProxEscala}</ul>
+    <ul>Ventas diarias para objetivo: ${ventasPorDiaProxEscala.toFixed(2)}</ul>
+    <ul>Comision: ${objComision}</ul>
     <br>
     <h3>20% extra para asegurar objetivo: ${parseInt((datosStorage[4]*1.2))}</h3>
-    <li>Ventas para asegurar objetivo: ${parseInt(((datosStorage[4]*1.2)-ventasTotal))}</li>
-    <li>Ventas diarias para asegurar objetivo: ${((datosStorage[4]*1.2)-ventasTotal)/datosStorage[3]}</li>
-    <li>Comision: ${objComision}</li>
+    <ul>Ventas para asegurar objetivo: ${parseInt(((datosStorage[4]*1.2)-ventasTotal))}</ul>
+    <ul>Ventas diarias para asegurar objetivo: ${(((datosStorage[4]*1.2)-ventasTotal)/datosStorage[3]).toFixed(2)}</ul>
+    <ul>Comision: ${objComision}</ul>
     `
     divContainterObjetivo2.append(impresionObjetivo2);
 }
@@ -209,9 +237,8 @@ function imprimirObjetivo2 () {
 
 /////limitar decimales en distribucion de % y en Ventas diarias para asegurar escala con 20% en objetivo1
 
-// agregar un toastify para cuando le damos a cargar datos
 
-// un sweet alert para cuando le damos a borrar todo
 
-// ver logica para que no puedan poner mas que numeros en los imput, si ponen otra cosa, un sweet alert para que revisen los datos requeridos
-
+//decimales en info general: distribucion de ventas, promedio 
+//en  obj1: diarias para prox escala en ambos
+//en obj2: mismo que obj1 para asegurar obj
